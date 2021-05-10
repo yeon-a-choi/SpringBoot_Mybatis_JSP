837b9923e4325ac7c958f07de4ac480365c40888
yeon-a-choi <banmu_@naver.com>
2021-05-10 22:03:49 +0900

    upload
    upload

---
+++ SpringBoot_Mybatis_JSP/src/main/java/com/ee/y1/home/HomeController.java
@@ -0,0 +1,24 @@
+package com.ee.y1.home;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * =======================
+ * JSP Controller
+ * =======================
+ */
+
+@Controller
+public class HomeController {
+	
+	@GetMapping("/")
+	public String home(Model model) {
+		
+		model.addAttribute("message", "JSP Project");
+		
+		return "index";
+	}
+
+}
